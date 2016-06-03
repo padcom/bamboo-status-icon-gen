0fe1c338087a83f5a3238473362f85e8cebf0567
Matthias Hryniszak <padcom@gmail.com>
2016-06-03 10:03:33 +0200

    Fixed wrong path to status image

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 
 app.get("/:token", function(request, response) {
-  response.end("[![Build Status](http://[bamboo]/plugins/servlet/wittfied/build-status/" + request.params.token + ")](http://[bamboo]/browse/" + request.params.token + ")");
+  response.end("[![Build Status](http://[bamboo]/plugins/servlet/wittified/build-status/" + request.params.token + ")](http://[bamboo]/browse/" + request.params.token + ")");
 });
 
 app.listen(process.env.PORT || 3000);
