af3031db9d12d99ba7a812e7b0b88cf2c40aed74
Matthias Hryniszak <padcom@gmail.com>
2016-06-03 10:27:07 +0200

    Added option to specify for which host this will be done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var app = require('express')();
 
 app.get("/:token", function(request, response) {
-  response.end("[![Build Status](http://[bamboo]/plugins/servlet/wittified/build-status/" + request.params.token + ")](http://[bamboo]/browse/" + request.params.token + ")");
+  var host = request.query.host || '[bamboo]';
+  response.end("[![Build Status](http://" + host + "/plugins/servlet/wittified/build-status/" + request.params.token + ")](http://" + host + "/browse/" + request.params.token + ")");
 });
 
 app.listen(process.env.PORT || 3000);
